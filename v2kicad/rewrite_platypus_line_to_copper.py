aebe2ecb5d1e5c9dbc6fe8215dbb49f2c9d9a3ec
twitchyliquid64 <twitchyliquid64@ciphersink.net>
2016-08-27 13:00:32 +1000

    final version with tiny vias - todo increase via size

--- v2kicad/rewrite_platypus_line_to_copper.py
+++ v2kicad/rewrite_platypus_line_to_copper.py
@@ -3,8 +3,8 @@ w = open("v2kicad.kicad_pcb.out", "w")
 with open("v2kicad.kicad_pcb", "r") as f:
     for line in f:
         if "gr_line" in line and "(layer F.SilkS) (width 0.5)" in line:
-            line = line.replace("(layer F.SilkS)", "(layer F.Mask)")
-	    w.write(line)
+            l2 = line.replace("(layer F.SilkS)", "(layer F.Mask)")
+	    w.write(l2)
             line = line.replace("(layer F.SilkS)", "(layer F.Cu)")
 	    w.write(line)
         else:
