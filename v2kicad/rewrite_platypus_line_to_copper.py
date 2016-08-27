381edde81745ad01b2dda9ff54c9be958852c30e
twitchyliquid64 <twitchyliquid64@ciphersink.net>
2016-08-27 12:36:31 +1000

    Done script to translate platypus graphic to front copper

---
+++ v2kicad/rewrite_platypus_line_to_copper.py
@@ -0,0 +1,13 @@
+w = open("v2kicad.kicad_pcb.out", "w")
+
+with open("v2kicad.kicad_pcb", "r") as f:
+    for line in f:
+        if "gr_line" in line and "(layer F.SilkS) (width 0.5)" in line:
+            line = line.replace("(layer F.SilkS)", "(layer F.Mask)")
+	    w.write(line)
+            line = line.replace("(layer F.SilkS)", "(layer F.Cu)")
+	    w.write(line)
+        else:
+            w.write(line)
+
+w.close()
